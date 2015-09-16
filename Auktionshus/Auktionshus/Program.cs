cfb128832e0f64f830d027e4ec505ed2e5018eb8
andreasbundgaard <andreasb94@live.dk>
2015-09-16 09:05:14 +0200

    Program klasse

--- Auktionshus/Auktionshus/Program.cs
+++ Auktionshus/Auktionshus/Program.cs
@@ -1,15 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 
-namespace Auktionshus
+namespace Auktionshuset
 {
-    class Program
+    internal class Program
     {
+        public static Auction _auction = new Auction();
+
         static void Main(string[] args)
         {
+            new SocketServer(11100);
+        }
+
+        private class SocketServer
+        {
+            private bool done = true;
+
+            public SocketServer(int port)
+            {
+                IPAddress ip = IPAddress.Parse("127.0.0.1");
+                var server = new TcpListener(ip, port);
+                server.Start();
+
+                Console.WriteLine("Server er klar på port " + port);
+
+                var auctionThread = new Thread(_auction.RunAuction);        //Starter en ny tråd til auktionshuset
+                auctionThread.Start();
+
+                while (true)
+                {
+                    Socket clientSocket = server.AcceptSocket();
+                    Console.WriteLine("Der er nu forbundet en byder...");
+
+                    var handler = new Clienthandler(clientSocket, _auction);
+
+                    var clientThread = new Thread(handler.RunClient);       //Starter en ny tråd til klienten
+                    clientThread.Start();
+                }
+            }
         }
     }
 }
